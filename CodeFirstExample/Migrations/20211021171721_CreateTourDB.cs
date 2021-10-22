bf81604ec3b2a94bc19683b5f1d76064d541f7ec
Lenovo <Lenovo@DESKTOP-TPH2NC6>
2021-10-22 21:53:27 +0530

    Add project files.

---
+++ CodeFirstExample/Migrations/20211021171721_CreateTourDB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFirstExample.Migrations
+{
+    public partial class CreateTourDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Passengers",
+                columns: table => new
+                {
+                    PID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TravelDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passengers", x => x.PID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Passengers");
+        }
+    }
+}
---
+++ CodeFirstExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstExample
+{
+    public class Passenger
+    {
+        [Key]
+        public int PID { get; set; }
+        public string PName { get; set; }
+        public DateTime? DOB { get; set; }
+        public DateTime? TravelDate { get; set; }
+    }
+    public class TourbusContext : DbContext
+    {
+        public TourbusContext()
+        {
+        }
+        public DbSet<Passenger> Passengers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            object p = optionsBuilder.UseSqlServer(@"Server=localhost;Database=TourDB;Trusted_Connection=True;");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (TourbusContext dbCtx = new TourbusContext())
+            {
+                var passenger = new Passenger()
+                {
+                    PName = "Priya Singh",
+                    DOB = new DateTime(),
+                    TravelDate = new DateTime()
+                };
+                dbCtx.Passengers.Add(passenger);
+                dbCtx.SaveChanges();
+            }
+        }
+    }
+
+}
